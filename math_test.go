45e43aa7f5d4e6474577557e314065d97534a1eb
Leonardo Camargo <leonardorcamargo@gmail.com>
2022-09-02 23:39:33 -0300

    first commit

---
+++ math_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.Errorf("Resuldado da soma é inválido: Resuldado %d. Esperado %d", total, 30)
+	}
+}
